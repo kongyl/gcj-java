b2a0e4a5b023aec1be781009c1d5a0109ebe4401
kongyl <kongyl@126.com>
2023-01-11 16:26:05 +0800

    init

---
+++ src/main/java/me/kongyl/gcjjava/GcjJavaApplication.java
@@ -0,0 +1,13 @@
+package me.kongyl.gcjjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcjJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcjJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kongyl/gcjjava/GcjJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kongyl.gcjjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcjJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
